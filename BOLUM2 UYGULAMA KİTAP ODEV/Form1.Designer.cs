a8c67f46a22e527d8df6f0968cb941ae2e85902f
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-10-03 16:31:39 +0300

    Add files via upload

---
+++ BOLUM2 UYGULAMA KİTAP ODEV/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace BOLUM2_UYGULAMA_KİTAP_ODEV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSayı1 = new System.Windows.Forms.TextBox();
+            this.txtSayı2 = new System.Windows.Forms.TextBox();
+            this.btnKarsılastır = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Brown;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(11, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1.sayı giriniz=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2.sayı giriniz=";
+            // 
+            // txtSayı1
+            // 
+            this.txtSayı1.Location = new System.Drawing.Point(119, 36);
+            this.txtSayı1.Name = "txtSayı1";
+            this.txtSayı1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayı1.TabIndex = 2;
+            // 
+            // txtSayı2
+            // 
+            this.txtSayı2.Location = new System.Drawing.Point(119, 67);
+            this.txtSayı2.Name = "txtSayı2";
+            this.txtSayı2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayı2.TabIndex = 3;
+            // 
+            // btnKarsılastır
+            // 
+            this.btnKarsılastır.BackColor = System.Drawing.Color.Turquoise;
+            this.btnKarsılastır.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKarsılastır.Location = new System.Drawing.Point(12, 116);
+            this.btnKarsılastır.Name = "btnKarsılastır";
+            this.btnKarsılastır.Size = new System.Drawing.Size(207, 54);
+            this.btnKarsılastır.TabIndex = 4;
+            this.btnKarsılastır.Text = "karşılaştır";
+            this.btnKarsılastır.UseVisualStyleBackColor = false;
+            this.btnKarsılastır.Click += new System.EventHandler(this.btnKarsılastır_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(263, 182);
+            this.Controls.Add(this.btnKarsılastır);
+            this.Controls.Add(this.txtSayı2);
+            this.Controls.Add(this.txtSayı1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSayı1;
+        private System.Windows.Forms.TextBox txtSayı2;
+        private System.Windows.Forms.Button btnKarsılastır;
+    }
+}
+
---
+++ BOLUM2 UYGULAMA KİTAP ODEV/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BOLUM2_UYGULAMA_KİTAP_ODEV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKarsılastır_Click(object sender, EventArgs e)
+        {
+            byte sayı1, sayı2;
+            sayı1 = Convert.ToByte(txtSayı1.Text);
+            sayı2 = Convert.ToByte(txtSayı2.Text);
+            if (sayı1 > sayı2)
+            {
+                MessageBox.Show("1.sayı2.sayıdan büyüktür");
+            }
+            if (sayı1 == sayı2)
+            {
+                MessageBox.Show("sayılar birbirıne eşitir");
+            }
+            if (sayı1 < sayı2)
+            {
+                MessageBox.Show("2.sayı1.sayıdan büyüktür");
+            }
+        }
+    }
+}
